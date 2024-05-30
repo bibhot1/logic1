581fed5a41dd42dee037bc168e381fb96611b468
Bibhot1 <bndazbvu.me12@gmail.com>
2024-05-31 07:16:11 +1200

    Add project files.

---
+++ logic1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+bool isRaining = true;
+bool hasUmbrella = true;
+
+//Logicak Operators
+
+//AND (&&)
+//OR (||)
+//NOT (!)
+
+if (isRaining || hasUmbrella)
+{
+    Console.WriteLine("Safe");
+}  
